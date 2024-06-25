d1f1f6aa1a3befd3d9868c984cc19c4820cc0f63
aparahuja123 <aparahuja1@gmail.com>
2024-06-25 17:13:59 +0800

    fix time zone

--- script/main.js
+++ script/main.js
@@ -1,8 +1,12 @@
-
 function updateTimer() {
-    const startDate = new Date('2023-01-01T00:00:00'); // Replace with your start date and time
+    // Set the start date in IST
+    const startDateIST = new Date('2023-05-21T00:00:00+05:30');
     const now = new Date();
-    const diff = now - startDate;
+
+    // Convert the current time to IST (UTC+5:30)
+    const nowIST = new Date(now.getTime() + (5.5 * 60 * 60 * 1000));
+
+    const diff = nowIST - startDateIST;
 
     const seconds = Math.floor((diff / 1000) % 60);
     const minutes = Math.floor((diff / 1000 / 60) % 60);
